cc3ce17c0a4fbc7eef5184ce35fe3d742bf4264e
José Eduardo Costa <47563160+duducosta@users.noreply.github.com>
2019-06-03 16:15:07 -0300

    Add project files.

---
+++ ChessGameConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessGameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
